948839a46b8cc3964fba0490689eaad8ec1f3a0b
Mike Silversides <mike.silversides@gmail.com>
2015-11-29 23:11:54 -0800

    more HUD files, should have been in prior commit

--- src/main/java/com/mikesilversides/mod1/proxy/ClientProxy.java
+++ src/main/java/com/mikesilversides/mod1/proxy/ClientProxy.java
@@ -3,7 +3,47 @@ package com.mikesilversides.mod1.proxy;
 import com.mikesilversides.mod1.init.TutorialBlocks;
 import com.mikesilversides.mod1.init.TutorialItems;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+
 public class ClientProxy extends CommonProxy{
+	
+	/**
+	   * Run before anything else. Read your config, create blocks, items, etc, and register them with the GameRegistry
+	   */
+	  public void preInit()
+	  {
+	    super.preInit();
+	    com.mikesilversides.mod1.hud_overlay.StartupClientOnly.preInitClientOnly();
+	  }
+	  
+	  public void init()
+	  {
+	    super.init();
+	    com.mikesilversides.mod1.hud_overlay.StartupClientOnly.initClientOnly();
+	  }
+
+	  public void postInit()
+	  {
+	    super.postInit();
+	    com.mikesilversides.mod1.hud_overlay.StartupClientOnly.postInitClientOnly();
+	  }
+	
+	  @Override
+	  public boolean playerIsInCreativeMode(EntityPlayer player) {
+	    if (player instanceof EntityPlayerMP) {
+	      EntityPlayerMP entityPlayerMP = (EntityPlayerMP)player;
+	      return entityPlayerMP.theItemInWorldManager.isCreative();
+	    } else if (player instanceof EntityPlayerSP) {
+	      return Minecraft.getMinecraft().playerController.isInCreativeMode();
+	    }
+	    return false;
+	  }
+
+	  
 	@Override
 	public void registerRenders(){
 		TutorialBlocks.registerRenders();
--- src/main/java/com/mikesilversides/mod1/proxy/CommonProxy.java
+++ src/main/java/com/mikesilversides/mod1/proxy/CommonProxy.java
@@ -1,6 +1,37 @@
 package com.mikesilversides.mod1.proxy;
 
-public class CommonProxy {
+import net.minecraft.entity.player.EntityPlayer;
+
+public abstract class CommonProxy {
+	/**
+	   * Run before anything else. Read your config, create blocks, items, etc, and register them with the GameRegistry
+	   */
+	  public void preInit()
+	  {
+		  com.mikesilversides.mod1.hud_overlay.StartupCommon.preInitCommon();
+	  }
+	  
+	  /**
+	   * Do your mod setup. Build whatever data structures you care about. Register recipes,
+	   * send FMLInterModComms messages to other mods.
+	   */
+	  public void init()
+	  {
+		  com.mikesilversides.mod1.hud_overlay.StartupCommon.initCommon();
+	  }
+	  
+	  /**
+	   * Handle interaction with other mods, complete your setup based on this.
+	   */
+	  public void postInit()
+	  {
+		  com.mikesilversides.mod1.hud_overlay.StartupCommon.postInitCommon();
+	  }
+	  
+	  // helper to determine whether the given player is in creative mode
+	  //  not necessary for most examples
+	  abstract public boolean playerIsInCreativeMode(EntityPlayer player);
+
 	public void registerRenders(){
 		
 	}
